b6b46d1a797bca514c7789eeaefdf862500058e6
Fortis298 <maxsimkolosov7@gmail.com>
2026-04-15 07:47:52 +0000

    Сделоно сообщениие в поиске а том что ничено не нашлось

--- frontend/home/js/searchRoom.js
+++ frontend/home/js/searchRoom.js
@@ -15,11 +15,27 @@ input.addEventListener('input', () => {
     .then(data => {
       let htmlResults = ''
       
-      for (let i = 0; i < data.result.length; i++) {
-         htmlResults += `
-          <div data-room-id="${data.result[i].room_id}">${data.result[i].name}</div>
-        `
+      if (!data.result.length) {
+        htmlResults =`<div class="not-results">Нет результатов</div>`
+        resultsContainer.innerHTML = htmlResults
+        resultsContainer.classList.add('active')
       }
+      
+      for (let i = 0; i < data.result.length; i++) {
+        let isPrivate = ``
+        
+        if (data.result[i].type == 'private') {
+          isPrivate = document.createElement('svg')
+          
+          isPrivate = `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="lock">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>`
+          }
+        
+         htmlResults += `<div data-room-id="${data.result[i].room_id}">${data.result[i].name}${isPrivate}</div>`
+      }
+      
       resultsContainer.innerHTML = htmlResults
       resultsContainer.classList.add('active')
     })
